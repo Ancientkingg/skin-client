a23ad4bc0c94dbf4ad42691decc72ac484b7e143
Ancientkingg <samuelmbruin@yahoo.com>
2022-03-19 19:01:55 +0100

    made some more changes

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@ use reqwest::Error;
 use serde_json::Value;
 use crossbeam;
 
+
+//? If bearer access token is greenlit switch over to 
+//? GET https://api.minecraftservices.com/minecraft/profile
+
 #[allow(dead_code)]
 fn get_skin_url(user_uuid: &str) -> Result<(String, bool), Error> {
     let url = format!("https://sessionserver.mojang.com/session/minecraft/profile/{user_uuid}");
@@ -13,11 +17,7 @@ fn get_skin_url(user_uuid: &str) -> Result<(String, bool), Error> {
     let skin_info: &str = std::str::from_utf8(&decoded).unwrap();
     let skin_url: Value = serde_json::from_str(skin_info).unwrap();
     let tex_url: &str = skin_url["textures"]["SKIN"]["url"].as_str().unwrap();
-    let slim = skin_url["textures"]["SKIN"]["metadata"]["model"].as_str();
-    let slim: bool = match slim {
-        Some(_) => true,
-        None => false
-    };
+    let slim: bool = true;//TODO set true as temp because my skin is slim
     Ok((tex_url.to_string(), slim))
 }
 
@@ -43,9 +43,9 @@ impl Rect {
 fn move_image(src: Rect, dst: Rect, mut img: DynamicImage) -> DynamicImage {
     let mut part_img = img.sub_image(src.x, src.y, src.width, src.height).to_image();
     if src != dst {
-        part_img = image::imageops::resize(&part_img, dst.width*2, dst.height, image::imageops::Nearest);
+        part_img = image::imageops::resize(&part_img, dst.width, dst.height, image::imageops::Nearest);
     }
-    img.copy_from(&part_img, dst.width, dst.height).unwrap();
+    img.copy_from(&part_img, dst.x, dst.y).unwrap();
     img
 }
 
@@ -64,11 +64,18 @@ fn process_skin(idx: usize, mut img: DynamicImage, slim: bool) {
     let img: DynamicImage = match idx {
         1 => {
             // Left arm
+            let mut mod_img = img;
             if !slim {
-                move_image(Rect::new(36, 48, 8, 4), Rect::new(8, 0, 16, 8), img)
+                mod_img = move_image(Rect::new(36, 48, 8, 4), Rect::new(8, 0, 16, 8), mod_img);
             } else {
-                move_image(Rect::new(36, 48, 6, 4), Rect::new(8, 0, 16, 8), img)
+                mod_img = move_image(Rect::new(36, 48, 3, 4), Rect::new(8, 0, 8, 8), mod_img);
+                mod_img = move_image(Rect::new(39, 48, 3, 4), Rect::new(16, 0, 8, 8), mod_img);
+                mod_img = move_image(Rect::new(36, 52, 3, 12), Rect::new(8, 8, 8, 8), mod_img);
+                mod_img = move_image(Rect::new(39, 52, 4, 12), Rect::new(16, 8, 8, 8), mod_img);
+                mod_img = move_image(Rect::new(32, 52, 4, 12), Rect::new(0, 8, 8, 8), mod_img);
+                mod_img = move_image(Rect::new(43, 52, 4, 12), Rect::new(24, 8, 8, 8), mod_img);
             }
+            mod_img
         }
         _ => DynamicImage::new_rgb8(1, 1),
     };
@@ -90,6 +97,8 @@ fn generate_skin(uuid: &str) {
             scope.spawn(move |_| process_skin(idx, img, slim));
         }
     }).unwrap();
+    // should also return if the player is slim or not
+    // dont wanna send another request because of ratelimiting so cache value of slim
 }
 
 #[test]
