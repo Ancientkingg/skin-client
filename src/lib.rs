963adb36a4a9d11dedbc1ac0a38467a44e1e6229
Ancientkingg <samuelmbruin@yahoo.com>
2022-04-02 10:40:35 +0200

    working version

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-use std::{io::Cursor, thread, time::Duration};
 use crossbeam;
 use image::{DynamicImage, GenericImage};
+use jni::objects::{JClass, JString};
+use jni::sys::{jboolean, jobjectArray, JNI_TRUE};
+use jni::JNIEnv;
 use rand::distributions::{Alphanumeric, DistString};
 use reqwest;
 use reqwest::Error;
 use serde_json::Value;
-use jni::JNIEnv;
-use jni::objects::{JClass, JString};
-use jni::sys::{jobject, jboolean, JNI_TRUE};
+use std::{io::Cursor, thread, time::Duration};
 
 const RETRY_TIMEOUT_MS: u64 = 150;
 
@@ -60,7 +60,6 @@ fn move_image(src: Rect, dst: Rect, mut img: DynamicImage) -> DynamicImage {
 ?RIGHT_LEG 4
 ?TORSO 5
 */
-
 
 fn process_skin(idx: usize, mut img: DynamicImage, slim: bool) -> DynamicImage {
     img.put_pixel(0, 0, image::Rgba([idx as u8, 0, 0, 255]));
@@ -237,7 +236,6 @@ fn process_skin(idx: usize, mut img: DynamicImage, slim: bool) -> DynamicImage {
     img
 }
 
-
 fn generate_skin(uuid: &str, slim: bool) -> Vec<(String, String)> {
     let tex_url = get_skin_url(uuid).unwrap();
     let image_bytes = reqwest::blocking::get(tex_url).unwrap().bytes().unwrap();
@@ -260,35 +258,44 @@ fn generate_skin(uuid: &str, slim: bool) -> Vec<(String, String)> {
                     .unwrap();
                 //TODO: need to add header with "Authorization: Bearer <your key>"
                 loop {
-                    let form = reqwest::blocking::multipart::Form::new()
-                        .text("variant", variant)
-                        .text(
-                            "name",
-                            Alphanumeric.sample_string(&mut rand::thread_rng(), 16),
-                        )
-                        .text("visibility", "1");
-                    let image = reqwest::blocking::multipart::Part::bytes(buf.clone())
-                        .mime_str("image/png")
-                        .unwrap()
-                        .file_name("joebama_skin.png");
-                    let form = form.part("file", image);
-                    let resp = client
-                        .post("https://api.mineskin.org/generate/upload")
-                        .multipart(form)
-                        .send()
-                        .unwrap();
+                    let resp = loop {
+                        let form = reqwest::blocking::multipart::Form::new()
+                            .text("variant", variant)
+                            .text(
+                                "name",
+                                Alphanumeric.sample_string(&mut rand::thread_rng(), 16),
+                            )
+                            .text("visibility", "1");
+                        let image = reqwest::blocking::multipart::Part::bytes(buf.clone())
+                            .mime_str("image/png")
+                            .unwrap()
+                            .file_name("joebama_skin.png");
+                        let form = form.part("file", image);
+                        let resp = client
+                            .post("https://api.mineskin.org/generate/upload")
+                            .multipart(form)
+                            .send();
+                        if let Ok(response) = resp {
+                            break response;
+                        }
+                        thread::sleep(Duration::from_millis(RETRY_TIMEOUT_MS / 4));
+                    };
                     let response: Value = resp.json().unwrap();
-                    if let Some(data) = response.get("data")?.get("texture") {
-                        let signature = data.get("signature").unwrap().as_str().unwrap();
-                        let value = data.get("value")?.as_str()?;
-                        return Some((signature.to_string(), value.to_string()));
+                    if let Some(data) = response.get("data") {
+                        if let Some(data) = data.get("texture") {
+                            let signature = data.get("signature").unwrap().as_str().unwrap();
+                            let value = data.get("value")?.as_str()?;
+                            return Some((signature.to_string(), value.to_string()));
+                        } else {
+                            thread::sleep(Duration::from_millis(RETRY_TIMEOUT_MS));
+                        }
                     } else {
                         thread::sleep(Duration::from_millis(RETRY_TIMEOUT_MS));
                     }
                 }
             });
             let data = handle.join().unwrap();
-            data_arr.push(data.unwrap());
+            data_arr.push(data.unwrap_or((String::new(), String::new())));
         }
         return data_arr;
     })
@@ -296,32 +303,53 @@ fn generate_skin(uuid: &str, slim: bool) -> Vec<(String, String)> {
     tex_data_arr
 }
 
-pub extern "system" fn java_generate_skin(env: JNIEnv, _class: JClass, input: JString, slim: jboolean) -> jobject {
+#[no_mangle]
+pub extern "system" fn Java_SGLib_generateSkin(
+    env: JNIEnv,
+    _class: JClass,
+    input: JString,
+    slim: jboolean,
+) -> jobjectArray {
     let uuid: String = env
         .get_string(input)
         .expect("Couldn't get java string!")
         .into();
     let slim = slim == JNI_TRUE;
     let processed_data = generate_skin(&uuid, slim);
-    let obj_example = env.new_object_array(2, env.find_class("java/lang/String").unwrap(), env.new_string("").unwrap()).unwrap();
+    let obj_example = env
+        .new_object_array(
+            2,
+            env.find_class("java/lang/String").unwrap(),
+            env.new_string("").unwrap(),
+        )
+        .unwrap();
     let class = env.get_object_class(obj_example).unwrap();
-    let outer = env.new_object_array(5, class,obj_example).unwrap();
+    let outer = env.new_object_array(5, class, obj_example).unwrap();
     for i in 0..5 {
-        let inner = env.new_object_array(2, env.find_class("java/lang/String").unwrap(), env.new_string("").unwrap()).unwrap();
-        env.set_object_array_element(inner, 0, env.new_string(&processed_data[i].0).unwrap()).unwrap();
-        env.set_object_array_element(inner, 1, env.new_string(&processed_data[i].1).unwrap()).unwrap();
-        env.set_object_array_element(outer, i as i32, inner).unwrap();
+        let inner = env
+            .new_object_array(
+                2,
+                env.find_class("java/lang/String").unwrap(),
+                env.new_string("").unwrap(),
+            )
+            .unwrap();
+        env.set_object_array_element(inner, 0, env.new_string(&processed_data[i].0).unwrap())
+            .unwrap();
+        env.set_object_array_element(inner, 1, env.new_string(&processed_data[i].1).unwrap())
+            .unwrap();
+        env.set_object_array_element(outer, i as i32, inner)
+            .unwrap();
     }
     outer
 }
 
-
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
-    
+
     #[test]
     fn it_works() {
-        generate_skin("a1f943fc512e4b379f780ede2c823707", true);
+        let output = generate_skin("a1f943fc512e4b379f780ede2c823707", true);
+        println!("{:?}", output);
     }
 }
